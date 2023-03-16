15a68e5bebd19ac8a5c60d03b5139755a27069c5
Kshitija Anil Patil <83634904+patilkshitija@users.noreply.github.com>
2023-03-16 21:15:54 +0530

    commit

---
+++ VueJS/hello-world/src/components/Simple.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+<template>
+    <div class="greetings">
+        <h1>Hello world</h1>
+        <h3>
+            you have succesfully created project with
+        </h3>
+    </div>
+</template>
+
+<style scoped>
+.greetings {
+    background-color: rgb(228, 197, 244);
+    padding: 20px;
+}
+
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
+}
